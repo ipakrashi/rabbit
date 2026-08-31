e0e6f31c20b6baeb8a0c94b53c67a40e0ae77776
ipakrashi <indranil.pakrashi@gmail.com>
2026-08-31 18:03:15 +0530

    04:38:30 Category Page Complete

--- frontend/src/components/products/SortOptions.jsx
+++ frontend/src/components/products/SortOptions.jsx
@@ -1,4 +1,28 @@
+import { useSearchParams } from 'react-router-dom'
+
 const SortOptions = () => {
-    return <div>SortOptions</div>
+    const [searchParams, setSearchParams] = useSearchParams()
+    const handleSortChange = (e) => {
+        const sortBy = e.target.value
+        searchParams.set('sortBy', sortBy)
+        setSearchParams(searchParams)
+    }
+
+    return (
+        <div className='mb-4 flex items-center justify-end'>
+            <select
+                name=''
+                id='sort'
+                onChange={handleSortChange}
+                value={searchParams.get('sortBy') || ''}
+                className='border p-2 rounded-md  focus:outline-none'
+            >
+                <option value=''>Default</option>
+                <option value='priceAsc'>Price: Low To High</option>
+                <option value='priceDesc'>Price: High To Low</option>
+                <option value='popularity'>Popularity</option>
+            </select>
+        </div>
+    )
 }
 export default SortOptions
